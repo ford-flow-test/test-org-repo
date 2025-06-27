1c471eb00c296835737fadb64fb4236fea59c726
fordpatsakorn <patsakorn_b@flowaccount.com>
2025-06-27 10:20:13 +0700

    add test 3

---
+++ AlwaysFailNUnitTest/UnitTest3.cs
@@ -0,0 +1,15 @@
+﻿namespace AlwaysFailNUnitTest;
+
+public class Tests3
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
